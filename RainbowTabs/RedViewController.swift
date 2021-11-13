ef56a3b3254dee1ed9700d391c708d50a4ce502a
kovs705 <smartkovsik@gmail.com>
2021-11-13 16:05:58 +0300

    new files and views

--- RainbowTabs/RedViewController.swift
+++ RainbowTabs/RedViewController.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
